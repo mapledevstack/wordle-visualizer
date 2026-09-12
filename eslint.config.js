15639d7bdec192214b1c5282014e192ce406f9aa
maple <maple.devstack@gmail.com>
2026-09-13 01:01:01 +0530

    ci: add Next.js checks

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import nextPlugin from "@next/eslint-plugin-next"
+import tseslint from "typescript-eslint"
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    ignores: [".next/**", "node_modules/**", "src/**"],
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["app/**/*.{js,jsx,ts,tsx}"],
+
+    plugins: {
+      "react-hooks": reactHooks,
+      "@next/next": nextPlugin,
     },
+
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...reactHooks.configs.flat.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+
+      // We'll deal with unused variables as we migrate.
+      "no-unused-vars": "off",
+    },
+
+    languageOptions: {
+      globals: globals.browser,
     },
   },
-])
+)
